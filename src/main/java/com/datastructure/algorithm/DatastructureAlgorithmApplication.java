1c35eafc098abbd65faf967873790738904911a9
gxing <gxing19@163.com>
2019-12-29 18:43:53 +0800

    rename

--- src/main/java/com/datastructure/algorithm/DatastructureAlgorithmApplication.java
+++ src/main/java/com/datastructure/algorithm/DatastructureAlgorithmApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DatastructureAlgorithmApplication {
+public class DataStructureAlgorithmApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DatastructureAlgorithmApplication.class, args);
+        SpringApplication.run(DataStructureAlgorithmApplication.class, args);
     }
 
 }
